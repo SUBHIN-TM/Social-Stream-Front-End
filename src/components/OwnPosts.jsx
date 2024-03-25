b2d6f85a79f1fb0635482befa4ae80b6f5d918c8
SUBHIN-TM <subhintm2018@gmail.com>
2024-03-25 09:42:21 +0530

    like icon changed

--- src/components/OwnPosts.jsx
+++ src/components/OwnPosts.jsx
@@ -95,8 +95,8 @@ const OwnPosts = ({ post, OWNPOSTS, setCommentRefreash }) => {
         <img className=" sm:w-11/12 sm:h-[300px] lg:w-11/12 w-10/12 h-[200px]" src={post.postImage} alt="" />
       </div>
       <div className="flex justify-around p-4">
-        {likeList.includes(userRedux.name) ? (<div className="flex"><span onClick={() => likefuction(post._id, post.userId)} className="cursor-pointer text-2xl"><BiSolidLike /> </span><span>({likeCount})</span></div>) :
-          (<div className="flex"><span onClick={() => likefuction(post._id, post.userId)} className="cursor-pointer text-2xl"><BiLike /></span ><span>({likeCount})</span></div>)
+        {likeList.includes(userRedux.name) ? (<div className="flex"><span onClick={() => likefuction(post._id, post.userId)} className="cursor-pointer text-2xl"><BiSolidLike /> </span><span>Liked ({likeCount})</span></div>) :
+          (<div className="flex"><span onClick={() => likefuction(post._id, post.userId)} className="cursor-pointer text-2xl"><BiLike /></span ><span>Like ({likeCount})</span></div>)
         }
 
 
